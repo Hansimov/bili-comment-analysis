c4fda003f5fa29b43d2d6c3b051d21466baefc71
Hansimov <591172499@qq.com>
2018-05-07 00:30:06 +0800

    + Add exportReplies function

--- test_getcomments.py
+++ test_getcomments.py
@@ -3,6 +3,8 @@ import json
 import math
 import glob
 from datetime import datetime
+import os
+import jieba, jieba.analyse
 
 # https://api.bilibili.com/x/v2/reply?oid=22755224&type=1&sort=0&nohot=1&pn=1
 
@@ -11,7 +13,16 @@ url_head = 'https://api.bilibili.com/x/v2/reply?'
 #         'Referer':'https://www.bilibili.com/video'
 #         }
 
-def getCommentPage(oid=22755224, xtype=1, sort=0, nohot=1, pn=1, is_get_page_num=0, is_get_req_json=0):
+
+def nameRepliesDir(oid):
+    replies_dir = 'replies_' + '{:0>10d}'.format(oid)
+    return replies_dir
+
+def getRepliesFile(oid, xtype=1, sort=0, nohot=1, pn=1, is_get_page_num=0, is_get_req_json=0):
+    replies_dir = nameRepliesDir(oid)
+    if not os.path.exists(replies_dir): 
+        os.mkdir(replies_dir)
+
     url_full = url_head \
                + '&oid='   + str(oid) \
                + '&type='  + str(xtype) \
@@ -19,7 +30,7 @@ def getCommentPage(oid=22755224, xtype=1, sort=0, nohot=1, pn=1, is_get_page_num
                + '&nohot=' + str(nohot) \
                + '&pn='    + str(pn)
 
-    print('> Getting comment page: {:0>4d}'.format(pn))
+    print('> Getting replies of av {:0>10d} at page: {:0>4d}'.format(oid,pn))
     req_info = requests.get(url_full)
     req_json = req_info.json()
 
@@ -28,7 +39,8 @@ def getCommentPage(oid=22755224, xtype=1, sort=0, nohot=1, pn=1, is_get_page_num
     except:
         pass
 
-    with open('./comments/{:0>4d}.json'.format(pn),'w', encoding='utf-8') as jsonfile:
+
+    with open((replies_dir+'/{:0>4d}.json').format(pn),'w', encoding='utf-8') as jsonfile:
         json.dump(req_json, jsonfile)
 
     if is_get_page_num == 1:
@@ -45,45 +57,62 @@ def getCommentPage(oid=22755224, xtype=1, sort=0, nohot=1, pn=1, is_get_page_num
 
 
 def getPageNum(req_json):
-    comment_count = req_json['data']['page']['count']
+    replies_count = req_json['data']['page']['count']
     page_size = req_json['data']['page']['size']
-    page_num = math.ceil(comment_count/page_size)
+    page_num = math.ceil(replies_count/page_size)
     return page_num
 
 
-def getAllComments(oid=22755224, xtype=1, sort=0, nohot=1, pn=1):
-    page_num, _ = getCommentPage(oid=oid, xtype=xtype, sort=sort, nohot=nohot, pn=pn, is_get_page_num=1)
+def getAllRepliesFiles(oid, xtype=1, sort=0, nohot=1, pn=1):
+    page_num, _ = getRepliesFile(oid=oid, xtype=xtype, sort=sort, nohot=nohot, pn=pn, is_get_page_num=1)
     for i in range(2, page_num + 1):
-        getCommentPage(oid=oid, xtype=xtype, sort=sort, nohot=nohot, pn=i)
+        getRepliesFile(oid=oid, xtype=xtype, sort=sort, nohot=nohot, pn=i)
 
 
-def combineJsonFiles(combined_jsonfile_name='allcomments.json'):
+def combineRepliesFiles(oid):
     # Issue with merging multiple JSON files in Python:
     #   https://stackoverflow.com/a/23520673/8328786
-    combined_jsonfile = []
+    combined_replies_file = []
     page_num = 0
-    for jsonfile in glob.glob("./comments/*.json"):
+    replies_dir = nameRepliesDir(oid)
+    combined_replies_file_name = 'combined_' + replies_dir + '.json'
+    for jsonfile in glob.glob(replies_dir+'/*.json'):
         page_num += 1
         with open(jsonfile, 'r') as infile:
-            combined_jsonfile.append(json.load(infile))
+            combined_replies_file.append(json.load(infile))
 
     # I may add a de-duplication step later.
     # Duplication will happen when new comment is added while crawling.
 
     print('+ Number of combined json files: {}'.format(page_num))
-    with open(combined_jsonfile_name, 'w') as outfile:
-         json.dump(combined_jsonfile, outfile)
+    with open(combined_replies_file_name, 'w') as outfile:
+         json.dump(combined_replies_file, outfile)
+
+    return combined_replies_file_name
 
 
+def exportReplies(oid, fmt='full',ext=''):
+    replies_dir = nameRepliesDir(oid)
+    combined_replies_file_name = 'combined_' + replies_dir + '.json'
 
-def processCombinedJsonFile(combined_jsonfile_name):
-    with open(combined_jsonfile_name, 'r') as combined_jsonfile:
-        replies_mdfile = open('replies.md','w')
-        combined_data = json.load(combined_jsonfile)
-        # print('|楼层|消息|点赞数|回复数|用户|')
-        print('| 楼层 | 时间 | 用户 | 消息 | 点赞数 | 回复数 |', file=replies_mdfile)
-        # print(' |  -   |  -   |   -    | -      | -    |')
-        print('| :-:  |  :-:   |  -   |   -  | :-:  | :-: |', file=replies_mdfile)
+    if fmt == 'full':
+        if ext == '':
+            ext = '.md'
+        replies_export_file_name = replies_dir + ext
+    elif fmt == 'only':
+        if ext == '':
+            ext = '.txt'
+        replies_export_file_name = replies_dir + ext
+
+    with open(combined_replies_file_name, 'r') as combined_replies_file:
+        replies_export_file = open(replies_export_file_name,'w')
+        combined_data = json.load(combined_replies_file)
+
+        if fmt == 'full':
+            # print('|楼层|消息|点赞数|回复数|用户|')
+            print('| 楼层 | 时间 | 用户 | 消息 | 点赞数 | 回复数 |', file=replies_export_file)
+            # print(' |  -   |  -   |   -    | -      | -    |')
+            print('| :-:  |  :-:   |  -   |   -  | :-:  | :-: |', file=replies_export_file)
 
         for page in range(0, len(combined_data)):
             current_page_replies = combined_data[page]['data']['replies']
@@ -100,14 +129,56 @@ def processCombinedJsonFile(combined_jsonfile_name):
                 like_num = current_reply['like']
                 replies_num = current_reply['rcount']
 
-                reply_str = '| {} | {} | {} | {} | {} | {} |'.format( 
-                    floor_str, time_str, uname_str, message_str, like_num, replies_num)
-                # print(reply_str)
-                print(reply_str, file = replies_mdfile)
-        replies_mdfile.close()
+                if fmt =='full':
+                    reply_str = '| {} | {} | {} | {} | {} | {} |'.format( 
+                        floor_str, time_str, uname_str, message_str, like_num, replies_num)
+                elif fmt == 'only':
+                    reply_str = message_str
+
+                print(reply_str, file = replies_export_file)
+
+        print('Exporting {} ...'.format(replies_export_file_name))
+        replies_export_file.close()
+
+        return replies_export_file_name
+
+def wordFrequency(topnum=-1):
+    pass
+
 
 if __name__ == '__main__':
-    combined_jsonfile_name = 'allcomments.json'
-    # getAllComments()
-    # combineJsonFiles(combined_jsonfile_name)
-    processCombinedJsonFile(combined_jsonfile_name)
+    this_oid = 22755224
+    # getAllRepliesFiles(this_oid)
+    # combineRepliesFiles(this_oid)
+    # exportReplies(this_oid, fmt='full')
+    replies_only_txt_name = exportReplies(this_oid, fmt='only')
+
+    jieba.load_userdict('bili_dict.txt')
+    with open(replies_only_txt_name,'r') as replies_only_txt:
+        # replies_only_txt_content = replies_only_txt.read()
+        # print('---')
+        # print(replies_only_txt_content)
+        # with open(nameRepliesDir(this_oid)+'_word_freq','w') as word_freq_file:
+        #     print(replies_only_txt_content,file=word_freq_file)
+
+        # 使用python对中文文档进行词频统计
+        #   https://blog.csdn.net/levy_cui/article/details/53129506
+
+        tag_dict = {}
+        with open('bili_dict.txt','r',encoding='utf-8') as bili_dict:
+            for line in bili_dict:
+                tag = line.strip('\n')
+                tag_dict[tag] = 0
+                # print(tag)
+
+        for line in replies_only_txt:
+            tags = jieba.analyse.extract_tags(line)
+            # print(line.strip('\n'))
+            for tag in tags:
+                if tag in tag_dict:
+                    tag_dict[tag] +=1
+
+                # print(tag, tag_dict[tag])
+        for tag in sorted(tag_dict, key=tag_dict.get, reverse=True):
+            print(tag, tag_dict[tag])
+        
